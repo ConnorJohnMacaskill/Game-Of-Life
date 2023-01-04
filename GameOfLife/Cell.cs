7f078d60002fc5c428af913ec4a9b3d548a6f8da
Connor John Macaskill <121974182+ConnorJohnMacaskill@users.noreply.github.com>
2023-01-04 20:45:09 +0000

    Initial Commit
    An old project.

---
+++ GameOfLife/Cell.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameOfLife
+{
+    class Cell
+    {
+        private bool alive;
+        private bool nextState;
+        private Cell[] neighbors;
+
+        public Cell(bool alive)
+        {
+            this.alive = alive;
+        }
+
+        public void setNeighbors(Cell[] neighbors)
+        {
+            this.neighbors = neighbors;
+        }
+
+        /// <summary>
+        /// Returns the state of the cell, in terms of being alive.
+        /// </summary>
+        public bool getState()
+        {
+            return alive;
+        }
+
+        public void applyNextState()
+        {
+            alive = nextState;
+        }
+
+        public int getValue()
+        {
+            if (alive == true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                return 1;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                return 0;
+            }
+        }
+
+        public void calculateState()
+        {
+            int liveCount = 0;
+
+            for (int i = 0; i < neighbors.Count(); i++)
+            {
+                if (neighbors[i].getState() == true)
+                {
+                    liveCount += 1;
+
+                }
+            }
+
+            //Rule 1 of the game of life.
+            if (liveCount < 2)
+            {
+                //Kill the cell.
+                nextState = false;
+            }
+
+
+
+            //Rule 3 of the game of life.
+            if (liveCount > 3)
+            {
+                //Kill the cell.
+                nextState = false;
+
+            }
+
+            //Rule 4 of the game of life.
+            if (liveCount == 3)
+            {
+                nextState = true;
+            }
+
+        }
+
+
+    }
+}
---
+++ GameOfLife/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace GameOfLife
+{
+    class Program
+    {
+        static int width = 60;
+        static int height = 25;
+        static int generation = 0;
+
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Cell[,] cells = new Cell[height, width];
+            bool exit = false;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (rand.Next(100) < 50)
+                    {
+                        cells[y, x] = new Cell(true);
+                    }
+                    else
+                    {
+                        cells[y, x] = new Cell(false);
+                    }
+                }
+            }
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    cells[y, x].setNeighbors(getNeighbors(cells, y, x));
+                }
+            }
+
+            int timer = 0;
+
+            while (exit == false)
+            {
+                if (timer == 0)
+                {
+                    generation += 1;
+                    DrawCells(cells);
+                    UpdateCells(cells);
+                    timer = 60000000;
+                }
+                else
+                {
+                    timer -= 1;
+                }
+            }
+
+            Console.Read();
+        }
+
+        static Cell[] getNeighbors(Cell[,] cells, int y, int x)
+        {
+            List<Cell> neighbors = new List<Cell>();
+
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    if (i != 0 || j != 0)
+                    {
+                        if ((y + i) > -1 && (y + i) < height)
+                        {
+                            if ((x + j) > -1 && (x + j) < width)
+                            {
+                                neighbors.Add(cells[(y + i), (x + j)]);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return neighbors.ToArray();
+        }
+
+        static void DrawCells(Cell[,] cells)
+        {
+            Console.Clear();
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Console.Write(cells[y, x].getValue());
+                }
+
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Generation : " + generation);
+        }
+
+        static void UpdateCells(Cell[,] cells)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    cells[y, x].calculateState();
+                }
+            }
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    cells[y, x].applyNextState();
+                }
+            }
+        }
+    }
+}
